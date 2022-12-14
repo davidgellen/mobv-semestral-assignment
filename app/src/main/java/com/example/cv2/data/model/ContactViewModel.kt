c8ef2ba91f6816b5785ffff6b3075703d9eace1b
David Gellen <gellen.david1@gmail.com>
2022-12-14 14:28:52 +0100

    Feat: contact repository rework

--- app/src/main/java/com/example/cv2/data/model/ContactViewModel.kt
+++ app/src/main/java/com/example/cv2/data/model/ContactViewModel.kt
@@ -1,19 +1,14 @@
 package com.example.cv2.data.model
 
-import android.content.Context
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.*
 import com.example.cv2.PubApplication
 import com.example.cv2.data.entity.Contact
-import com.example.cv2.data.response.ContactResponseBody
-import com.example.cv2.mapper.ContactResponseToEntityMapper
 import com.example.cv2.database.repository.ContactRepository
-import com.example.cv2.service.RetrofitFriendApi
-import com.example.cv2.utils.ConnectivityUtils
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class ContactViewModel(
@@ -21,51 +16,25 @@ class ContactViewModel(
     private val application: PubApplication
 ): ViewModel() {
 
-    private val context by lazy { application.applicationContext }
-
     private var _contacts = MutableLiveData<MutableList<Contact>>()
-
-    private val connectivityUtils = ConnectivityUtils()
 
     val entries: LiveData<MutableList<Contact>>
         get() = _contacts
 
-    fun getApplication() : PubApplication {
+    fun getApplication(): PubApplication {
         return application
-    }
-
-    fun setContacts(
-        list: MutableList<Contact>
-    ) {
-        _contacts.value = list
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
     @DelicateCoroutinesApi
     fun fetchAllFriends() {
-
-        val sharedPreference = context.getSharedPreferences(
-            "PREFERENCE_NAME", Context.MODE_PRIVATE)
-        val accessToken = "Bearer " + (sharedPreference?.getString("access", "defaultAccess") ?: "defaultAccess")
-        val uid = sharedPreference?.getString("uid", "defaultUid") ?: "defaultUid"
-        if (connectivityUtils.isOnline(context!!)) {
-            viewModelScope.launch(Dispatchers.Main) {
-                val response: List<ContactResponseBody> = RetrofitFriendApi.RETOROFIT_SERVICE
-                    .allFriends(accessToken , uid)
-                val contacts: MutableList<Contact> = ContactResponseToEntityMapper().entryListToPubList(
-                    response.toMutableList(), uid.toLong())
-                contactRepository.insert(contacts)
-                _contacts.value = contacts
-
-            }
-        } else {
-            Log.e("NO INTERNET", "LOADING CONTACTS FROM DATABASE")
-            val contacts: MutableList<Contact> = contactRepository.getAllByUserId(uid.toInt())
-            _contacts.value = contacts
-        }
-
+        GlobalScope.launch(Dispatchers.Main) {
+            val resp = contactRepository.getAll()
+            _contacts.value = resp
         }
     }
+
+}
 
 
 class ContactViewModelFactory(
--- app/src/main/java/com/example/cv2/database/repository/ContactRepository.kt
+++ app/src/main/java/com/example/cv2/database/repository/ContactRepository.kt
@@ -1,14 +1,30 @@
 package com.example.cv2.database.repository
 
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import com.example.cv2.database.dao.ContactDao
 import com.example.cv2.data.entity.Contact
+import com.example.cv2.data.response.ContactResponseBody
+import com.example.cv2.mapper.ContactResponseToEntityMapper
+import com.example.cv2.service.RetrofitFriendApi
+import com.example.cv2.utils.ConnectivityUtils
 
 class ContactRepository(
-    private val contactDao: ContactDao
+    private val contactDao: ContactDao,
+    private val context: Context
 ) {
 
-    fun getAll(): MutableList<Contact> {
-        return contactDao.getAll()
+    @RequiresApi(Build.VERSION_CODES.M)
+    suspend fun getAll(): MutableList<Contact> {
+        if (ConnectivityUtils().isOnline(context)) {
+            Log.i("GET ALL FRIENDS", "FROM SERVICE")
+            return fetchFromService()
+        } else {
+            Log.i("GET ALL FRIENDS", "FROM DATABASE")
+            return contactDao.getAll()
+        }
     }
 
     fun getAllByUserId(userId: Int): MutableList<Contact> {
@@ -19,6 +35,19 @@ class ContactRepository(
         contacts: List<Contact>
     ) {
         contactDao.insert(contacts);
+    }
+
+    private suspend fun fetchFromService() : MutableList<Contact> {
+        val sharedPreference = context.getSharedPreferences(
+            "PREFERENCE_NAME", Context.MODE_PRIVATE)
+        val accessToken = "Bearer " + (sharedPreference?.getString("access", "defaultAccess") ?: "defaultAccess")
+        val uid = sharedPreference?.getString("uid", "defaultUid") ?: "defaultUid"
+        val response: List<ContactResponseBody> = RetrofitFriendApi.RETOROFIT_SERVICE
+            .allFriends(accessToken , uid)
+        val contacts: MutableList<Contact> = ContactResponseToEntityMapper().entryListToPubList(
+            response.toMutableList(), uid.toLong())
+        insert(contacts)
+        return contacts
     }
 
 }
